9ff633cd8ad84db75dd9824de38cbe1946acd9a2
Felix Tan <felixtan92@gmail.com>
2020-11-08 02:42:30 +0800

    Add EntryCard component

---
+++ client/src/components/EntryCard.tsx
@@ -0,0 +1,42 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  IconButton,
+  Stack,
+  Tag,
+} from "@chakra-ui/core";
+import React from "react";
+
+interface EntryCardProps {
+  title: string;
+  text?: string | null;
+  isHearted: boolean;
+}
+
+export const EntryCard: React.FC<EntryCardProps> = ({
+  title,
+  text,
+  isHearted,
+}) => {
+  return (
+    <Box padding={4} maxW="20rem" borderWidth="1px" rounded=".25rem">
+      <Flex direction="row" justifyContent="space-between">
+        <Heading mb={2} as="h3" size="md">
+          {title}
+        </Heading>
+        <IconButton
+          aria-label="Heart entry"
+          icon="heart"
+          isRound
+        />
+      </Flex>
+      {text && <Box mb={2}>{text}</Box>}
+      <Stack flexWrap="wrap" direction="row">
+        <Tag>idea</Tag>
+        <Tag>animals</Tag>
+        <Tag>...</Tag>
+      </Stack>
+    </Box>
+  );
+};
