cd7338c66c7a534d2c2b3db56727730f7a1edde5
Felix Tan <felixtan92@gmail.com>
2020-11-07 19:21:08 +0800

    Add variant prop to define max width of body

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import { Box } from '@chakra-ui/core';
-import { NavBar } from './NavBar';
+import React from "react";
+import { Box } from "@chakra-ui/core";
+import { NavBar } from "./NavBar";
+
+type LayoutVariant = "small" | "regular" | "large";
 
 interface LayoutProps {
+  variant?: LayoutVariant;
 }
 
-export const Layout: React.FC<LayoutProps> = ({ children }) => {
-    return (
-      <Box>
-        <NavBar />
-        <Box paddingX={4}>
-          {children}
-        </Box>
-      </Box>
-    );
-}
+export const Layout: React.FC<LayoutProps> = ({ children, variant }) => {
+  let maxWidth;
+  switch(variant) {
+    case "small":
+      maxWidth = "40rem";
+      break;
+    case "regular":
+      maxWidth = "80rem";
+      break;
+    default:
+      maxWidth = "100%";
+  }
+
+  return (
+    <Box>
+      <NavBar />
+      <Box marginX="auto" paddingX={4} maxW={maxWidth}>{children}</Box>
+    </Box>
+  );
+};
