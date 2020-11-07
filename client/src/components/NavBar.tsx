9ac5449f771fcc97cb0f578b4962004b8275ad6e
Felix Tan <felixtan92@gmail.com>
2020-11-07 17:47:41 +0800

    Add NavBar component

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Avatar, Box, Button, Flex, Heading, Icon, IconButton, Input, InputGroup, InputLeftElement } from '@chakra-ui/core';
+
+interface NavBarProps {
+
+}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  return (
+    <Flex padding={4} align="center">
+      <Heading mr={4} as="h1">
+        TIW
+      </Heading>
+      <InputGroup>
+        <InputLeftElement children={<Icon name="search" />} />
+        <Input type="search" placeholder="Search for an entry...">
+        </Input>
+      </InputGroup>
+      <Box flex={1} mr={4} />
+      <Button mr={4}>
+        Create Entry
+      </Button>
+      <IconButton aria-label="User options" icon="user" isRound />
+    </Flex>
+  );
+}
