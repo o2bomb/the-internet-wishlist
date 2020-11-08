a4b65fb72db9f9af135173e146a2d90757a8d8d0
Felix Tan <felixtan92@gmail.com>
2020-11-08 16:53:50 +0800

    Add working pagination on client

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,31 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
+import { PaginatedEntries } from "../generated/graphql";
 
 import theme from "../theme";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache()
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          entries: {
+            keyArgs: [], // tells Apollo to just lump all entries together (instead of grouping them by limit, cursor, hasMore)
+            merge(existing: PaginatedEntries | undefined, incoming: PaginatedEntries): PaginatedEntries {
+              // https://www.apollographql.com/docs/react/caching/cache-field-behavior/#the-merge-function
+              // existing: stuff thats already in the cache
+              // incoming: stuff coming from the server
+              return {
+                ...incoming,
+                entries: [...(existing?.entries || []), ...incoming.entries]
+              }
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 function MyApp({ Component, pageProps }) {
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,76 @@
-import React from "react"
-import { Layout } from "../components/Layout"
+import { Box, Button, Stack } from "@chakra-ui/core";
+import React from "react";
+import { EntryCard } from "../components/EntryCard";
+import { Layout } from "../components/Layout";
+import {
+  useGetEntriesQuery
+} from "../generated/graphql";
 
-const Index = () => (
-  <Layout>
-    <div>
-      hello world!
-    </div>
-  </Layout>
-)
+const Index = () => {
+  const { loading, error, data, fetchMore } = useGetEntriesQuery({
+    variables: {
+      limit: 2,
+    },
+    notifyOnNetworkStatusChange: true, // sets loading to true whenever query is rerun
+  });
 
-export default Index
+  if (!data && loading) {
+    return <Layout>Loading entries...</Layout>;
+  }
+
+  if (error) {
+    console.log(error);
+    return <Layout>An error occurred while loading entries</Layout>;
+  }
+
+  return (
+    <Layout>
+      <Stack spacing={4}>
+        {data?.entries.entries.map((e, index) => (
+          <Box key={index}>
+            <EntryCard {...e} isHearted={false} />
+          </Box>
+        ))}
+      </Stack>
+      <Button
+        isLoading={loading}
+        onClick={() => {
+          if (data?.entries.hasMore) {
+            fetchMore({
+              variables: {
+                cursor: data?.entries.cursor,
+              },
+              // OLD WAY Of UPDATING QUERY IN CACHE
+              // updateQuery: (
+              //   previousValue,
+              //   { fetchMoreResult }
+              // ): GetEntriesQuery => {
+              //   if (!fetchMoreResult) {
+              //     return previousValue as GetEntriesQuery;
+              //   }
+
+              //   return {
+              //     __typename: "Query",
+              //     entries: {
+              //       __typename: "PaginatedEntries",
+              //       cursor: (fetchMoreResult as GetEntriesQuery).entries.cursor,
+              //       hasMore: (fetchMoreResult as GetEntriesQuery).entries
+              //         .hasMore,
+              //       entries: [
+              //         ...previousValue.entries.entries,
+              //         ...(fetchMoreResult as GetEntriesQuery).entries.entries,
+              //       ],
+              //     },
+              //   };
+              // },
+            });
+          }
+        }}
+      >
+        Load more
+      </Button>
+    </Layout>
+  );
+};
+
+export default Index;
