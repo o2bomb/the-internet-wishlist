ec3aad1df2320fce07dc28e3039a6a9676ce9697
Felix Tan <felixtan92@gmail.com>
2020-11-09 02:39:08 +0800

    Implement custom InputField component using formik

---
+++ client/src/components/InputField.tsx
@@ -0,0 +1,48 @@
+import { FormControl, FormErrorMessage, FormHelperText, FormLabel, Input, Textarea } from "@chakra-ui/core";
+import { useField } from "formik";
+import React, { InputHTMLAttributes } from "react";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+  type?: "email" | "password" | "text";
+  helperText?: string;
+  textArea?: boolean;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  type,
+  helperText,
+  textArea,
+  required,
+  size: _,
+  ...props
+}) => {
+  const [field, { error }] = useField(props); // hooks up input to formik
+
+  const InputOrTextArea = textArea ? Textarea : Input
+
+  return (
+    <FormControl isRequired={required} isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <InputOrTextArea
+        type={type}
+        id={field.name}
+        aria-describedby={helperText ? `${field.name}-helper-text` : undefined}
+        {...props}
+        {...field}
+      />
+      {helperText ? (
+        <FormHelperText id={`${field.name}-helper-text`}>
+          {helperText}
+        </FormHelperText>
+      ) : null}
+      {
+        error ? (
+          <FormErrorMessage>{error}</FormErrorMessage>
+        ) : null
+      }
+    </FormControl>
+  );
+};
