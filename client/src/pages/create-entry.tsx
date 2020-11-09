0d01f54afc7deedc3d5506e0c9e0a489392d5208
Felix Tan <felixtan92@gmail.com>
2020-11-10 02:40:20 +0800

    Update entries cache when new entry is created

--- client/src/pages/create-entry.tsx
+++ client/src/pages/create-entry.tsx
@@ -4,14 +4,49 @@ import { Button } from "@chakra-ui/core";
 import { Form, Formik } from "formik";
 import { Layout } from "../components/Layout";
 import { InputField } from "../components/InputField";
-import { useCreateEntryMutation } from "../generated/graphql";
+import {
+  Entry,
+  PaginatedEntries,
+  useCreateEntryMutation,
+} from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
+import { gql } from "@apollo/client";
 
 interface CreateEntryProps {}
 
 const CreateEntry: React.FC<CreateEntryProps> = ({}) => {
   const router = useRouter();
-  const [createEntry] = useCreateEntryMutation();
+  const [createEntry] = useCreateEntryMutation({
+    update: (cache, { data }) => {
+      const newEntry = data?.createEntry.entry;
+      if (data?.createEntry.entry) {
+        cache.modify({
+          fields: {
+            entries: (existing: PaginatedEntries | undefined): PaginatedEntries => {
+              // Update the cache when a new entry is created
+              // https://www.apollographql.com/docs/react/data/mutations/#making-all-other-cache-updates
+              const newEntryRef = cache.writeFragment({
+                data: newEntry,
+                fragment: gql`
+                  fragment NewEntry on Entry {
+                    id
+                  }
+                `,
+              });
+
+              let cursor = existing?.cursor || newEntry?.createdAt || "";
+              let hasMore = existing?.hasMore || true;
+              return {
+                cursor,
+                hasMore,
+                entries: [newEntryRef as any, ...(existing?.entries || [])]
+              }
+            },
+          },
+        });
+      }
+    },
+  });
 
   return (
     <Layout variant="small">
@@ -24,15 +59,15 @@ const CreateEntry: React.FC<CreateEntryProps> = ({}) => {
           const response = await createEntry({
             variables: {
               title: values.title,
-              text: values.text || null
-            }
+              text: values.text || null,
+            },
           });
 
           if (response.data?.createEntry.errors) {
             setErrors(toErrorMap(response.data.createEntry.errors));
           } else if (response.data?.createEntry.entry) {
             // Redirect user to home page
-            router.push("/"); 
+            router.push("/");
           }
         }}
       >
@@ -40,7 +75,9 @@ const CreateEntry: React.FC<CreateEntryProps> = ({}) => {
           <Form>
             <InputField name="title" label="Title" placeholder="I wish..." />
             <InputField name="text" label="Description" />
-            <Button mt={4} isLoading={isSubmitting} type="submit">Create entry</Button>
+            <Button mt={4} isLoading={isSubmitting} type="submit">
+              Create entry
+            </Button>
           </Form>
         )}
       </Formik>
