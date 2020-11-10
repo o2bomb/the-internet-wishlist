bc7e02fee01c39ff772bbb6b77f546a92df130a7
Felix Tan <felixtan92@gmail.com>
2020-11-10 20:14:30 +0800

    Display creator name and points on EntryCard

--- client/src/components/EntryCard.tsx
+++ client/src/components/EntryCard.tsx
@@ -1,5 +1,12 @@
-import { gql } from "@apollo/client";
-import { Box, Flex, Heading, IconButton, Stack, Tag, useToast } from "@chakra-ui/core";
+import {
+  Box,
+  Flex,
+  Heading,
+  IconButton,
+  Stack,
+  Tag,
+  useToast,
+} from "@chakra-ui/core";
 import React from "react";
 import {
   RegularEntryFragment,
@@ -11,45 +18,52 @@ interface EntryCardProps {
 }
 
 export const EntryCard: React.FC<EntryCardProps> = ({ entry }) => {
-  const { id, title, isHearted, text } = entry;
+  const { id, title, isHearted, text, points, creator } = entry;
   const toast = useToast();
   const [heartEntry] = useHeartEntryMutation();
   return (
     <Box padding={4} maxW="20rem" borderWidth="1px" rounded=".25rem">
-      <Flex direction="row" justifyContent="space-between">
-        <Heading mb={2} as="h3" size="md">
-          {title}
-        </Heading>
-        <IconButton
-          onClick={async () => {
-            await heartEntry({
-              variables: {
-                id,
-                deleteHeart: isHearted,
-              },
-            }).catch((e) => {
-              // error occurred, notify user
-              console.log(e);
+      <Stack spacing={1}>
+        <Flex direction="row" justifyContent="space-between">
+          <Heading as="h3" size="md">
+            {title}
+          </Heading>
+          <IconButton
+            onClick={async () => {
+              await heartEntry({
+                variables: {
+                  id,
+                  deleteHeart: isHearted,
+                },
+              }).catch((e) => {
+                // error occurred, notify user
+                console.log(e);
 
-              toast({
-                title: "You must be logged in to like an entry!",
-                status: "error",
-                duration: 1000,
-                isClosable: true,
-              })
-            });
-          }}
-          variantColor={isHearted ? "red" : undefined}
-          aria-label="Heart entry"
-          icon="heart"
-          isRound
-        />
-      </Flex>
-      {text && <Box mb={2}>{text}</Box>}
-      <Stack flexWrap="wrap" direction="row">
-        <Tag>idea</Tag>
-        <Tag>animals</Tag>
-        <Tag>...</Tag>
+                toast({
+                  title: "You must be logged in to like an entry!",
+                  status: "error",
+                  duration: 1000,
+                  isClosable: true,
+                });
+              });
+            }}
+            variantColor={isHearted ? "red" : undefined}
+            aria-label="Heart entry"
+            icon="heart"
+            isRound
+          />
+        </Flex>
+        {text && <Box>{text}</Box>}
+        <Box color="gray.500" letterSpacing="wide" fontSize="sm">
+          {points ? `${points} points` : "No points"} &bull; By{" "}
+          {creator ? creator.displayName : "Anonymous"}
+        </Box>
+        <Box></Box>
+        <Stack flexWrap="wrap" direction="row">
+          <Tag>idea</Tag>
+          <Tag>animals</Tag>
+          <Tag>...</Tag>
+        </Stack>
       </Stack>
     </Box>
   );
