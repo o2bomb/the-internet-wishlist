00eb3c99bdf8aab57f641088f2dbd983d12dae0c
Felix Tan <felixtan92@gmail.com>
2020-11-11 18:20:00 +0800

    Forgot to commit this to make viewport provider work

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
 import { PaginatedEntries } from "../generated/graphql";
-
+import { ViewportProvider } from "../utils/ViewportProvider";
 import theme from "../theme";
 
 const client = new ApolloClient({
@@ -13,14 +13,17 @@ const client = new ApolloClient({
         fields: {
           entries: {
             keyArgs: [], // tells Apollo to just lump all entries together (instead of grouping them by limit, cursor, hasMore)
-            merge(existing: PaginatedEntries | undefined, incoming: PaginatedEntries): PaginatedEntries {
+            merge(
+              existing: PaginatedEntries | undefined,
+              incoming: PaginatedEntries
+            ): PaginatedEntries {
               // https://www.apollographql.com/docs/react/caching/cache-field-behavior/#the-merge-function
               // existing: stuff thats already in the cache
               // incoming: stuff coming from the server
               return {
                 ...incoming,
-                entries: [...(existing?.entries || []), ...incoming.entries]
-              }
+                entries: [...(existing?.entries || []), ...incoming.entries],
+              };
             },
           },
         },
@@ -32,12 +35,14 @@ const client = new ApolloClient({
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <ColorModeProvider>
-          <CSSReset />
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ThemeProvider>
+      <ViewportProvider>
+        <ThemeProvider theme={theme}>
+          <ColorModeProvider>
+            <CSSReset />
+            <Component {...pageProps} />
+          </ColorModeProvider>
+        </ThemeProvider>
+      </ViewportProvider>
     </ApolloProvider>
   );
 }
