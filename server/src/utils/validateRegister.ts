42a342a1b93af74e912457a8f251ecfa3f057764
Felix Tan <felixtan92@gmail.com>
2020-11-12 19:06:36 +0800

    Move email validation further down

--- server/src/utils/validateRegister.ts
+++ server/src/utils/validateRegister.ts
@@ -2,14 +2,6 @@ import { FieldError } from "src/resolvers/FieldError";
 import { UsernamePasswordInput } from "src/resolvers/user";
 
 export const validateRegister = (options: UsernamePasswordInput): FieldError[] | null => {
-  if (!options.email.includes("@")) {
-    return [
-      {
-        field: "email",
-        message: "Invalid email",
-      },
-    ];
-  }
   if (options.username.includes("@")) {
     return [
       {
@@ -23,6 +15,14 @@ export const validateRegister = (options: UsernamePasswordInput): FieldError[] |
       {
         field: "username",
         message: "Username length must be greater than 2",
+      },
+    ];
+  }
+  if (!options.email.includes("@")) {
+    return [
+      {
+        field: "email",
+        message: "Invalid email",
       },
     ];
   }
