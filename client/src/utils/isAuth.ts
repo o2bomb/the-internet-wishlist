670f8f45514bfcb030defadc10f1c904c8ce758c
Felix Tan <felixtan92@gmail.com>
2020-11-30 14:35:41 +0800

    Add isAuth util for redirecting user to login page if they're not logged in

---
+++ client/src/utils/isAuth.ts
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useMeQuery } from "../generated/graphql";
+
+export const isAuth = () => {
+  const {data, loading} = useMeQuery();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !data?.me) {
+      router.replace("/login?next=" + router.pathname);
+    }
+  }, [data, router]);
+};
