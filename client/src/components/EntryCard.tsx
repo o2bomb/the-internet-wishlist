e2263060390232b0654aa843b131ea23a4915c93
Felix Tan <felixtan92@gmail.com>
2020-12-03 21:17:13 +0800

    Restructure EntryCard markup

--- client/src/components/EntryCard.tsx
+++ client/src/components/EntryCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NextLink from "next/link";
-import { Box, Flex, Heading, Link, Tag } from "@chakra-ui/react";
+import { Box, Flex, Heading, Link, Stack, Tag } from "@chakra-ui/react";
 import { RegularEntryFragment, useMeQuery } from "../generated/graphql";
 import { HeartButton } from "./HeartButton";
 import { TagContainer } from "./TagContainer";
@@ -13,8 +13,14 @@ export const EntryCard: React.FC<EntryCardProps> = ({ entry }) => {
   const { id, title, text, points, creator, tags, creatorId } = entry;
   const { data } = useMeQuery();
   return (
-    <Box padding={4} maxW="40rem" borderWidth="1px" rounded=".25rem">
-      <Flex mb={2} direction="row" justifyContent="space-between">
+    <Flex
+      justifyContent="space-between"
+      padding={4}
+      maxW="40rem"
+      borderWidth="1px"
+      rounded=".25rem"
+    >
+      <Stack spacing={2}>
         <NextLink href="/entry/[id]" as={`/entry/${id}`} passHref>
           <Link>
             <Heading as="h2" size="lg">
@@ -22,16 +28,22 @@ export const EntryCard: React.FC<EntryCardProps> = ({ entry }) => {
             </Heading>
           </Link>
         </NextLink>
+        {text && <Box>{text}</Box>}
+        <Box color="gray.500" letterSpacing="wide" fontSize="sm">
+          {points ? `${points} points` : "No points"} &bull; By{" "}
+          {creator ? creator.displayName : "Anonymous"}
+        </Box>
+        {tags.length > 0 ? (
+          <TagContainer>
+            {tags.map((t, index) => (
+              <Tag key={index}>{t.displayName}</Tag>
+            ))}
+          </TagContainer>
+        ) : null}
+      </Stack>
+      <Flex>
         {data?.me?.id === creatorId ? null : <HeartButton entry={entry} />}
       </Flex>
-      {text && <Box mb={2}>{text}</Box>}
-      <Box mb={2} color="gray.500" letterSpacing="wide" fontSize="sm">
-        {points ? `${points} points` : "No points"} &bull; By{" "}
-        {creator ? creator.displayName : "Anonymous"}
-      </Box>
-      <TagContainer>
-        {tags ? tags.map((t, index) => <Tag key={index}>{t.displayName}</Tag>) : null}
-      </TagContainer>
-    </Box>
+    </Flex>
   );
 };
