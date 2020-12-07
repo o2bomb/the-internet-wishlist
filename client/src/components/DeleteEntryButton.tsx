12013fc62b2fb80ca50152b957c1c28ee4ad66fc
Felix Tan <felixtan92@gmail.com>
2020-12-07 18:09:38 +0800

    Add DeleteEntryButton component

---
+++ client/src/components/DeleteEntryButton.tsx
@@ -0,0 +1,110 @@
+import { DeleteIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React from "react";
+import { PaginatedEntries, useDeleteEntryMutation } from "../generated/graphql";
+
+interface DeleteEntryButtonProps {
+  id: number;
+  redirect?: boolean;
+}
+
+export const DeleteEntryButton: React.FC<DeleteEntryButtonProps> = ({
+  id,
+  redirect,
+}) => {
+  const router = useRouter();
+  const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [deleteEntry, { error }] = useDeleteEntryMutation({
+    update: (cache, { data }) => {
+      if (!error) {
+        cache.modify({
+          fields: {
+            entries: (
+              existing: PaginatedEntries | undefined,
+              { readField }
+            ) => {
+              // Remove entry from cache after deleteing
+              // https://stackoverflow.com/a/63514349
+              return {
+                ...existing,
+                entries: existing?.entries.filter(
+                  (e) => id !== readField("id", e)
+                ),
+              };
+            },
+          },
+        });
+      }
+    },
+  });
+
+  const ConfirmDelete = (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Delete entry?</ModalHeader>
+        <ModalBody>This action cannot be undone!</ModalBody>
+        <ModalFooter>
+          <Button mr={3} onClick={onClose}>
+            Cancel
+          </Button>
+          <Button
+            onClick={async () => {
+              try {
+                await deleteEntry({
+                  variables: {
+                    id,
+                  },
+                });
+
+                // send user back to index page
+                if (redirect) {
+                  router.push("/");
+                }
+
+                toast({
+                  title: "Entry deleted successfully!",
+                  status: "success",
+                  duration: 1000,
+                  isClosable: true,
+                });
+              } catch (e) {
+                // Failed to delete entry
+                if (redirect) {
+                  router.push("/");
+                }
+              }
+            }}
+            colorScheme="red"
+          >
+            Delete entry
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+
+  return (
+    <>
+      <IconButton
+        onClick={onOpen}
+        icon={<DeleteIcon />}
+        aria-label="Delete entry"
+      />
+      {ConfirmDelete}
+    </>
+  );
+};
--- client/src/components/EditDeleteEntryButtons.tsx
+++ client/src/components/EditDeleteEntryButtons.tsx
@@ -1,20 +1,8 @@
-import React from "react";
+import { Button } from "@chakra-ui/react";
 import NextLink from "next/link";
-import {
-  Button,
-  IconButton,
-  useDisclosure,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  useToast,
-} from "@chakra-ui/react";
-import { DeleteIcon } from "@chakra-ui/icons";
-import { PaginatedEntries, useDeleteEntryMutation, useMeQuery } from "../generated/graphql";
-import { useRouter } from "next/router";
+import React from "react";
+import { useMeQuery } from "../generated/graphql";
+import { DeleteEntryButton } from "./DeleteEntryButton";
 
 interface EditDeleteEntryButtonsProps {
   id: number;
@@ -25,77 +13,14 @@ export const EditDeleteEntryButtons: React.FC<EditDeleteEntryButtonsProps> = ({
   id,
   creatorId,
 }) => {
-  const router = useRouter();
-  const toast = useToast();
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const { data } = useMeQuery();
-  const [deleteEntry, { error }] = useDeleteEntryMutation({
-    update: (cache, { data }) => {
-      if (!error) {
-        cache.modify({
-          fields: {
-            entries: (
-              existing: PaginatedEntries | undefined, { readField }
-             ) => {
-              // Remove entry from cache after deleteing
-              // https://stackoverflow.com/a/63514349
-              return {
-                ...existing,
-                entries: existing?.entries.filter(e => id !== readField("id", e))
-              };
-            },
-          },
-        });
-      }
-    },
-  });
-
-  const ConfirmDelete = (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Delete entry?</ModalHeader>
-        <ModalBody>This action cannot be undone!</ModalBody>
-        <ModalFooter>
-          <Button mr={3} onClick={onClose}>
-            Cancel
-          </Button>
-          <Button onClick={async () => {
-            try {
-              await deleteEntry({
-                variables: {
-                  id
-                }
-              })
-
-              // send user back to index page
-              router.push("/");
-  
-              toast({
-                title: "Entry deleted successfully!",
-                status: "success",
-                duration: 1000,
-                isClosable: true,
-              });
-            } catch (e) {
-              // Failed to delete entry
-              router.push("/");
-            }
-          }} colorScheme="red">
-            Delete entry
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-  );
 
   return data?.me?.id === creatorId ? (
     <>
       <NextLink href="/entry/edit/[id]" as={`/entry/edit/${id}`} passHref>
         <Button mr={4}>Edit entry</Button>
       </NextLink>
-      <IconButton onClick={onOpen} icon={<DeleteIcon />} aria-label="Delete entry" />
-      {ConfirmDelete}
+      <DeleteEntryButton id={id} redirect />
     </>
   ) : null;
 };
